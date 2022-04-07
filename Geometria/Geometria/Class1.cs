30a8b306d61dc00199d8944b45502dfa05dd789a
Simone Serpi <simoneserpi@hotmail.it>
2022-04-07 18:14:12 +0200

    Fine_Bonus

--- Geometria/Geometria/Class1.cs
+++ Geometria/Geometria/Class1.cs
@@ -10,9 +10,9 @@ namespace MyGeometry
 	{
 		private int baseRettangolo;
 		private int altezzaRettangolo;
-		
 
-	   // ----------- ISTANZA -----------
+
+		// ----------- ISTANZA -----------
 		public Rettangolo(int baseRettangolo, int altezzaRettangolo)
 		{
 			this.baseRettangolo = baseRettangolo;
@@ -26,7 +26,7 @@ namespace MyGeometry
 			Area = baseRettangolo * altezzaRettangolo;
 			return Area;
 		}
-		public double calcolaPerimetro ()
+		public double calcolaPerimetro()
 		{
 			double Perimetro = 0;
 			Perimetro = (baseRettangolo + altezzaRettangolo) * 2;
@@ -44,17 +44,28 @@ namespace MyGeometry
 		}
 		public void Disegna()
 		{
-
-			Console.Write("  ");
-			for (int i = 0; i<baseRettangolo; i++)
-			{
-				Console.Write("--");
-			}
-			Console.WriteLine("  ");
 			for (int i = 0; i < altezzaRettangolo; i++)
 			{
-				Console.WriteLine(" | ");
+				for (int j = 0; j < baseRettangolo; j++)
+				{
+					if (i == 0 || i == (altezzaRettangolo - 1))
+					{
+						Console.Write("-");
+
+					}
+					else if (j == 0 || j == (baseRettangolo - 1))
+					{
+						Console.Write("|");
+					}
+					else
+					{
+						Console.Write(" ");
+					}
+				}
+				Console.Write("\n");
+
 			}
+
 		}
 	}
 }
